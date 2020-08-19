cf5ab3d8564679e9d0a9b46f688696cfab3f6288
Anuj Patel <anuj@remine.com>
2020-08-19 12:31:11 -0400

    cleanup brew install

--- mac-setup.sh
+++ mac-setup.sh
@@ -18,123 +18,121 @@ brew cask install homebrew/cask-versions/adoptopenjdk8
 brew install python3
 brew install zsh
 
-brew install \
-    antibody \
-    aria2 \
-    awscli \
-    bash \
-    bat \
-    black \
-    brew-cask-completion \
-    cmatrix \
-    coreutils \
-    curl \
-    docker-completion \
-    docker-compose-completion \
-    dos2unix \
-    fasd \
-    fd \
-    findutils \
-    flake8 \
-    fzf \
-    gawk \
-    gcc \
-    git \
-    gnu-getopt \
-    gnu-indent \
-    gnu-sed \
-    gnu-tar \
-    go \
-    grep \
-    htop \
-    httpie \
-    imagemagick \
-    jq \
-    kafka \
-    kompose \
-    kops \
-    krb5 \
-    kubernetes-cli \
-    lzo \
-    moreutils \
-    mtr \
-    nano \
-    neofetch \
-    nmap \
-    node \
-    nvm \
-    nzbget \
-    openssh \
-    openssl \
-    parquet-tools \
-    pigz \
-    pip-completion \
-    pipenv \
-    pipx \
-    poetry \
-    postgresql \
-    pre-commit \
-    pyenv \
-    python \
-    python3 \
-    readline \
-    rename \
-    ripgrep \
-    rsync \
-    speedtest-cli \
-    starship \
-    terminal-notifier \
-    terraform \
-    tmux \
-    tree \
-    unrar \
-    vim \
-    watch \
-    wget \
-    xsv \
-    xvid \
-    xz \
-    youtube-dl \
-    zookeeper \
-    zsh \
-    zsh-completions \
-    zstd
+brew install antibody
+brew install aria2
+brew install autoconf
+brew install aws-iam-authenticator
+brew install awscli
+brew install bash
+brew install bat
+brew install black
+brew install brew-cask-completion
+brew install cmatrix
+brew install coreutils
+brew install curl
+brew install docker-completion
+brew install docker-compose-completion
+brew install dos2unix
+brew install entr
+brew install fasd
+brew install fd
+brew install fdupes
+brew install findutils
+brew install flake8
+brew install fzf
+brew install gawk
+brew install gcc
+brew install gettext
+brew install git
+brew install gitmoji
+brew install gnu-getopt
+brew install gnu-indent
+brew install gnu-sed
+brew install gnu-tar
+brew install gnutls
+brew install go
+brew install graphviz
+brew install grep
+brew install htop
+brew install httpie
+brew install imagemagick
+brew install jq
+brew install kubernetes-cli
+brew install lzo
+brew install mtr
+brew install nano
+brew install ncurses
+brew install neofetch
+brew install nmap
+brew install node
+brew install openjdk
+brew install openssl
+brew install pgcli
+brew install pipenv
+brew install pipx
+brew install pkg-config
+brew install poetry
+brew install postgresql
+brew install pre-commit
+brew install procs
+brew install pyenv
+brew install readline
+brew install ripgrep
+brew install sd
+brew install shellcheck
+brew install snappy
+brew install speedtest-cli
+brew install starship
+brew install tealdeer
+brew install terminal-notifier
+brew install terraform
+brew install tesseract
+brew install tmux
+brew install tree
+brew install watch
+brew install wget
+brew install xmlto
+brew install xsv
+brew install xvid
+brew install xz
+brew install youtube-dl
+brew install zlib
+brew install zsh-completions
+brew install zstd
+
 
 brew tap homebrew/cask-fonts
 
-brew cask install \
-    appcleaner \
-    authy \
-    datagrip \
-    dbeaver-community \
-    docker \
-    evernote \
-    firefox \
-    font-hack-nerd-font \
-    geekbench \
-    gifox \
-    gimp \
-    google-chrome \
-    iina \
-    iterm2 \
-    keybase \
-    kodi \
-    ksdiff \
-    macdown \
-    microsoft-teams \
-    parsec \
-    pycharm \
-    royal-tsx \
-    sensiblesidebuttons \
-    slack \
-    snowflake-snowsql \
-    spotify \
-    the-unarchiver \
-    tunnelblick \
-    visual-studio-code \
-    vlc \
-    whatsapp \
-    yacreader
+
+brew cask install appcleaner
+brew cask install authy
+# brew cask install datagrip
+brew cask install dbeaver-community
+brew cask install docker
+brew cask install evernote
+brew cask install firefox
+brew cask install font-hack-nerd-font
+brew cask install gifox
+brew cask install gimp
+brew cask install google-chrome
+brew cask install iina
+brew cask install iterm2
+brew cask install ksdiff
+brew cask install macdown
+# brew cask install microsoft-teams
+brew cask install parsec
+# brew cask install pycharm
+# brew cask install royal-tsx
+brew cask install sensiblesidebuttons
+brew cask install slack
+brew cask install snowflake-snowsql
+brew cask install spotify
+brew cask install the-unarchiver
+brew cask install tunnelblick
+brew cask install visual-studio-code
+brew cask install vlc
+brew cask install whatsapp
+brew cask install yacreader
 
 # iTerm integration
 curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
